60f67fba91ef0a00972f7b72eb2d8c8c39652e08
Carnagiul-42 <piquerue@student.42.fr>
2018-08-06 07:04:06 +0200

    remove libft

--- libft/.sh_tool/leaks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -ge 1 ]
-then
-	PROCESSID=$(pgrep -f $1)
-	#PROCESSID=$(ps | grep "wolf3d" | grep -v "grep" | sed 's/^[ ]*//g' | cut -d" " -f1)
-	leaks -q $PROCESSID
-else
-	echo "Usage: ./leaks.sh <projectname>"
-fi

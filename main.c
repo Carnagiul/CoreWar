eb302300dd831a305936d9d05bb8cdd5fde358da
Carnagiul-42 <piquerue@student.42.fr>
2018-08-07 06:39:00 +0200

    start reading of .s files

--- main.c
+++ main.c
@@ -6,15 +6,78 @@
 /*   By: piquerue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/06 06:54:32 by piquerue          #+#    #+#             */
-/*   Updated: 2018/08/07 05:06:54 by piquerue         ###   ########.fr       */
+/*   Updated: 2018/08/07 06:38:20 by piquerue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_corewar.h>
 
+typedef struct		s_asm
+{
+	char			*filename;
+	char			*name;
+	char			*comment;
+}					t_asm;
+
+static int	read_write_champ_header_name(char *line, t_asm *fasm)
+{
+	char		**tab;
+	int			i;
+	int			error;
+
+	error = 0;
+	tab = ft_strsplitwhitespace(line);
+	i = 0;
+	while (tab[i] && i <= 3)
+		i++;
+	if (i != 2)
+		error = 1;
+	if (error == 0)
+		if (ft_strcmp(tab[0], ".name") != 0 || ft_strlen(tab[1]) <= 2)
+			error = 1;
+	i = -1;
+	while (tab[1][++i] && error == 0)
+		if (((i == 0 || (i + 1) == (int)ft_strlen(tab[1])) &&
+					tab[1][i] != '"') || (ft_isalnum(tab[1][i]) == 0))
+			error = 1;
+	if (error == 0)
+		fasm->name = ft_strdup_from_to(tab[1], 1, ft_strlen(tab[1]) - 2);
+	free_char_ss(tab);
+	return ((error == 1) ? 0x4 : 0);
+}
+
+static int	read_write_champ_header_comment(char *line, t_asm *fasm)
+{
+	(void)line;
+	(void)fasm;
+	return (0);
+}
+
+static int	read_write_champ_header(char **lines, t_asm *fasm)
+{
+	int		ret;
+	int		ret2;
+
+	if (!(lines[0]))
+		return (0x4);
+	ret = read_write_champ_header_name(lines[0], fasm);
+	if (!(lines[1]))
+		return (0x5);
+	ret2 = read_write_champ_header_comment(lines[1], fasm);
+	if (ret != 0)
+		return (ret);
+	return (ret2);
+}
 
 static void	read_write_champ(char **lines, char *name)
 {
+	t_asm	*fasm;
+
+	fasm = ft_malloc(sizeof(*fasm));
+	fasm->filename = ft_strdup_from_to_offset(name, 0, ft_strlen(name) - 1, 3);
+	ft_strcat(fasm->filename, "cor");
+	ft_printf("new name is %s\n", fasm->filename);
+	read_write_champ_header(lines, fasm);
 	(void)lines;
 	(void)name;
 }
@@ -36,6 +99,8 @@ static int	create_champion(int fd, char *file)
 	contents = ft_get_content_file_fd(fd);
 	lines = ft_strsplit(contents, '\n');
 	read_write_champ(lines, file);
+	free_char_ss(lines);
+	free(contents);
 	return (0x0);
 }
 
@@ -58,6 +123,10 @@ static int	do_display_error_asm(int error)
 		ft_printf("usage ; ./asm <filename.s>\n@RYou're file is not good\n@@");
 	if (error == 0x3)
 		ft_printf("usage ; ./asm <filename.s>\n@RCan't verify extension of file\n@@");
+	if (error == 0x4)
+		ft_printf("\n@RWrong parsing on file with\n.name must be on top of file like :\n@B.name       \"my name\"@@\n");
+	if (error == 0x5)
+		ft_printf("\n@RWrong parsing on file with\n.name must be on second line of file like :\n@B.comment       \"my descr\"@@\n");
 	return (error);
 }
 
@@ -78,5 +147,7 @@ int		main(int argc, char **argv)
 	else if (ft_strcmp(argv[0], "./corewar") == 0)
 		result = do_vm(argc, argv);
 	do_display_error_asm(result);
+	while (1)
+		;
 	return (result);
 }

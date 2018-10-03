7eafd7733335b3dc2dc6df97f66234e19e5aa9a9
Sophie NASSOUR <snassour@student.42.fr>
2018-10-03 22:57:55 +0200

    repo clean

---
+++ asm/tests_perso/toto.s
@@ -0,0 +1,26 @@
+
+.comment "      iii         "		#;ewkf
+; toto        
+
+
+
+.name "     ooo        " #opewit
+                  
+				ld 							%222,			   				 r1				;:
+add r5, r4, r9	
+
+ld :buff    ,    r1
+
+st r8, 4
+
+#--==:			
+	#.comment " rfrf "
+
+tir: sti	r1,%:tirb,%1
+       
+sti r2,%67,%1
+
+ii:
+
+live %1
+
